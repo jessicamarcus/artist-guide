14f912755252bfc870bf25877897b32f2bac74ae
Jessica Marcus <jessica.marcus@gmail.com>
2014-10-11 23:23:04 -0400

    adding some pages, minor updates

---
+++ js/ill.googlemap.js
@@ -0,0 +1,61 @@
+// Google Maps Scripts
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 17,
+        center: new google.maps.LatLng(42.341236, -71.066167),
+        disableDefaultUI: true,
+        scrollwheel: false,
+        draggable: true,
+
+        styles: [
+            {
+                "stylers": [
+                    {
+                        "hue": "#ff1a00"
+                    },
+                    {
+                        "invert_lightness": true
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 33
+                    },
+                    {
+                        "gamma": 0.5
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#2d333c"
+                    },
+                    {
+                        "lightness": 10
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            }
+        ]
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
--- js/illuminus.js
+++ js/illuminus.js
@@ -27,7 +27,6 @@ if ($('#tumblrfeed').length > 0) {
         $.each(data['posts'], function (i, posts) {
             var jsonPost = data['posts'][i],
                 htmlPost = template(jsonPost);
-
             $('#tumblrfeed').append(htmlPost);
         });
     });
@@ -40,80 +39,3 @@ if ($('#landing').length
 
     $('body').css({'background-image': 'url(' + randBg + ')'});
 }
-
-// form validation
-//$(document).ready(function () {
-//    $('#volunteerForm').bootstrapValidator({
-//        submitButtons: 'button[type="submit"]',
-////        excluded: [':disabled'],
-//        feedbackIcons: {
-//            valid: 'glyphicon glyphicon-ok',
-//            invalid: 'glyphicon glyphicon-remove',
-//            validating: 'glyphicon glyphicon-refresh'
-//        },
-//        fields: {
-//            firstname: {
-//                selector: '[placeholder="First Name"]',
-//                validators: {
-//                    notEmpty: {
-//                        message: 'Required'
-//                    }
-//                }
-//            },
-//            lastname: {
-//                selector: '[placeholder="Last Name"]',
-//                validators: {
-//                    notEmpty: {
-//                        message: 'Required'
-//                    }
-//                }
-//            },
-//            q4_email4: {
-//                validators: {
-//                    notEmpty: {
-//                        message: 'Required'
-//                    },
-//                    emailAddress: {
-//                        message: 'Valid email address required'
-//                    }
-//                }
-//            },
-//            phoneNumber: {
-//                selector: '[placeholder="617-555-1212"]',
-//                validators: {
-//                    notEmpty: {
-//                        message: 'Required'
-//                    },
-//                    phone: {
-//                        country: 'US',
-//                        message: 'Valid telephone number required'
-//                    }
-//                }
-//            },
-//            q7_areasOf: {
-//                selector: '[class="areas"]',
-//                validators: {
-//                    choice: {
-//                        min: 1,
-//                        max: 10,
-//                        message: 'Please choose at least one option'
-//                    }
-//                }
-//            },
-//            q8_whatIs: {
-//                validators: {
-//                    notEmpty: {
-//                        message: 'Required'
-//                    }
-//                }
-//            }
-//        }
-//    });
-//});
-//
-//$('#volunteerModal').on('shown.bs.modal', function () {
-//    $('#volunteerForm').find('[placeholder="First Name"]').focus();  // Initial focus on the username field
-//});
-//$('volunteerModal').on('success.form.bv', function (e) {
-//    e.preventDefault();  // Prevent submission unless required fields are completed
-//});
